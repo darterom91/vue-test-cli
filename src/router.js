5b42eeafb02c8ccdf3cc1d9cc7d87ec7d8ec7796
David Artero <d.arterom@gmail.com>
2019-06-19 16:13:24 +0200

    vuex router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -11,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
     },
     {
       path: '/about',
@@ -20,6 +19,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/servicios',
+      name: 'servicios',
+      component: () => import(/* webpackChunkName: "about" */ './views/Servicios.vue')
+    },
+    {
+      path: '/fotos/:id',
+      name: 'fotos',
+      component: () => import(/* webpackChunkName: "about" */ './views/Fotos.vue')
     }
   ]
 })
