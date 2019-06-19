ae696070e341be15a8f390692aa9c7915dbf29c7
David Artero <d.arterom@gmail.com>
2019-06-19 17:52:18 +0200

    vuex: NAVBAR

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+
       <router-link to="/">Home</router-link>
       |
       <router-link to="/about">About</router-link>
@@ -9,6 +10,25 @@
       |
       <router-link to="/Fotos/1">fotos</router-link>
     </div>
+
+<div>
+  <b-container>
+    <b-navbar toggleable="md" type="dark" variant="success">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/" exact>Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+          <b-nav-item to="/servicios">servicios</b-nav-item>
+          <b-nav-item to="/Fotos/1">fotos</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </b-container>
+</div>
     <router-view/>
   </div>
 </template>
