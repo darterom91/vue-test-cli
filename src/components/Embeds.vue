4b2ca99ede3d355ec7604277166aa41a58e725fe
David Artero <d.arterom@gmail.com>
2019-06-20 14:50:19 +0200

    vuex: slides-images responsive

---
+++ src/components/Embeds.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-embed
+    type="iframe"
+    aspect="21by9"
+    src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+    allowfullscreen>
+    </b-embed>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'embeds'
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="about text-primary">
-    <img alt="Vue logo" src="../assets/AboutUs.jpg">
     <h1>This is an about page</h1>
+      <b-img :src="require('../assets/AboutUs.jpg')"  fluid class="ml-1 mr-1 mt-3 mb-2" alt="Fluid image"></b-img>
+      <b-img src="https://picsum.photos/1024/480/?image=55" fluid class="ml-1 mr-2 mt-3 mb-2" alt="Fluid image"></b-img>
+    <embeds class="m-5"/>
   </div>
 </template>
+
+<script>
+import embeds from '@/components/Embeds.vue'
+export default {
+  name: 'about',
+  data() {
+      return {
+        mainProps: { blank: true, width: 75, height: 75, class: 'm1' }
+      }
+    },
+  components: {
+    embeds
+  }
+}
+</script>
